5bb3867cad7c4c1dd1c31786e989c389990a47fd
dexkj <97032499+dexkj@users.noreply.github.com>
2022-05-26 22:28:53 +0800

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
-    message = "Example Django project on vercel"
-    return HttpResponse(message)
+    # message = "Example Django project on vercel"
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
